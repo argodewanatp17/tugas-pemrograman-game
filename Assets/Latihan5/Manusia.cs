07236be8a6f008a9c7a88d6882011c89f128cfbf
argodewanatp17 <71372423+argodewanatp17@users.noreply.github.com>
2020-09-28 21:17:30 +0700

    Tugas Pertemuan 3
    Tugas Pertemuan 3 Pemrograman Game
    
    Argo Dewana
    01716147155

---
+++ Assets/Latihan5/Manusia.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manusia : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Makan() 
+   
+        { 
+            Debug.Log("Perlu Makan"); 
+        } 
+    
+    public void Tidur() 
+    
+        { 
+            Debug.Log("Perlu Tidur"); 
+        } 
+}
+
